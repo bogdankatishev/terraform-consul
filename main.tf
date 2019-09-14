b4c7e9189bb1f51f05519884588ebd74477f889c
bogdan <bogdan_katishev@hotmail.com>
2019-09-14 16:57:32 +0200

    Split up mounts into 2 because of upgrade that is not recursive

--- main.tf
+++ main.tf
@@ -22,21 +22,21 @@ resource "docker_service" "consul" {
         container_spec {
             image = "consul"
 
-            mounts = [
-                {
-                    target      = "/var/run/docker.sock"
-                    source      = "/var/run/docker.sock"
-                    type        = "bind"
-                    read_only   = true
-                },
-                {
-                    source      = "${docker_volume.consul_data.name}"
-                    target      = "/var/consul"
-                    type        = "volume"
-                    read_only   = false
-                },
+            mounts = {
+                          target      = "/var/run/docker.sock"
+                          source      = "/var/run/docker.sock"
+                          type        = "bind"
+                          read_only   = true
+                     }
 
-            ]
+            mounts = {
+                          source      = "${docker_volume.consul_data.name}"
+                          target      = "/var/consul"
+                          type        = "volume"
+                          read_only   = false
+                     },
+
+
         }
         networks     = ["${docker_network.private_network.name}"]
     }
